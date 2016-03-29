91b6bf03baa9ed5fc4b18261a5a7133676b672eb
ChuhuiZhang <chuhui0812@gmail.com>
2016-03-29 00:35:43 -0400

    Update MyDAOException.java

--- MyDAOException.java
+++ MyDAOException.java
@@ -1,10 +1,5 @@
 package model;
 
-/**
- * @author Chuhui Zhang (chuhuiz@andrew.cmu.edu)
- * Course number: 08-672
- * Dec 5, 2015
- */
 public class MyDAOException extends Exception {
 	private static final long serialVersionUID = 1L;
 
